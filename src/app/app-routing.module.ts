51634c956a14f9fb31a5eceac56d64578eca6fb1
Gerardo Jimenez Ronquillo <ssweb1@unete.org>
2018-07-12 15:35:00 -0500

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,54 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './componentes/login/login.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { SidenavComponent } from './componentes/sidenav/sidenav.component';
+import { FaqComponent } from './componentes/faq/faq.component';
+import { TecnicaComponent } from './componentes/tecnica/tecnica.component';
+import { ConectividadComponent } from './componentes/conectividad/conectividad.component';
+import { NoticiasComponent } from './componentes/noticias/noticias.component';
+import { ContenidosComponent } from './componentes/contenidos/contenidos.component';
+import { ManualesComponent } from './componentes/manuales/manuales.component';
+
+const routes: Routes = [
+  {
+    path: '', 
+    component: LoginComponent
+  },
+  {
+    path: 'home', 
+    component: HomeComponent
+  },
+  {
+    path: 'sidenav', 
+    component: SidenavComponent
+  },
+  {
+    path: 'faq', 
+    component: FaqComponent
+  },
+  {
+    path: 'tecnica', 
+    component: TecnicaComponent
+  },
+  {
+    path: 'conectividad', 
+    component: ConectividadComponent
+  },
+  {
+    path: 'noticias', 
+    component: NoticiasComponent
+  },
+  {
+    path: 'contenidos', 
+    component: ContenidosComponent
+  },
+  {
+    path: 'manuales', 
+    component: ManualesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
